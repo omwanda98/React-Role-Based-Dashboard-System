f040515e7624e3ef250c4b648f6c4937d791d418
omwanda98 <omwandajeyy@gmail.com>
2024-07-28 20:41:11 -0700

    build for deployment

---
+++ build/precache-manifest.47f3a68da95b5ea3622b1afb4caae716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26818e1c19fb20ea3fd5a3a58a9c6ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb58abe15c79219ec909",
+    "url": "/static/css/main.97e3f34a.chunk.css"
+  },
+  {
+    "revision": "f510374455ebb7b0e237",
+    "url": "/static/js/2.f1688bc8.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/static/js/2.f1688bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb58abe15c79219ec909",
+    "url": "/static/js/main.a5add4fe.chunk.js"
+  },
+  {
+    "revision": "23c4ef23b7f8ab4670a5",
+    "url": "/static/js/runtime-main.f855baa5.js"
+  }
+]);
